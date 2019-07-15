6cf10b80045ac1e001c7d5b284ef7c037c2600c7
Marc Schlossberg <mr.schlossberg@gmail.com>
2019-07-15 13:21:53 -0700

    Add styles to the right column

--- frontend/src/components/main/details_widget.jsx
+++ frontend/src/components/main/details_widget.jsx
@@ -10,8 +10,8 @@ class DetailsWidget extends React.Component {
                 <h3>Controls</h3>
 
                 <p>
-                    Eliminate the viruses before your beaker overflows.  Matching
-                    <strong>any 4 pieces of the same color</strong> will eliminate
+                    Eliminate the viruses before your beaker overflows.  Lining up
+                     <strong> any 4 single (1x1) pieces of the same color</strong> will eliminate
                     those pieces, be they virus or pill.  Bonus points for chaining
                     combos! (probably)
                 </p>
@@ -19,23 +19,28 @@ class DetailsWidget extends React.Component {
                 <section className="game-pad">
                     
                     
-                    <div className="arrows">
+                    <div className="direction-pad">
                         {/* top row */}
                         <div className="blank"></div>
-                        <div className="blank"></div>
+                        <div>
+                            {/* down NEED TO CSS ROTATE*/}
+                            <div className="arrow">&#8682;</div>
+                        </div>
                         <div className="blank"></div>
                         
                         {/* middle row */}
                         <div>
                             {/* left arrow */}
-                            <div className="arrow">&#8592;</div>
+                            <div className="arrow">&#8678;</div>
                         </div>
 
-                        <div className="blank"></div>
+                        <div className="middle">
+                            <span></span>
+                        </div>
                     
                         <div>
                             {/* right arrow */}
-                            <div className="arrow">&#10145;</div>
+                            <div className="arrow">&#8680;</div>
                         </div>
 
                         {/* bottom row */}
@@ -51,19 +56,14 @@ class DetailsWidget extends React.Component {
                     </div>
 
                     <div className="select-start">
-                       <button>pause</button>
-                       <button>reset</button>
+
+                            <button>&#10602;</button>
+                            <button>&#8634;</button>
                     </div>
 
                     <div className="buttons">
-                        <div>
-                            <button>A</button>
-                            <span>rL</span>
-                        </div>
-                        <div>
-                            <button>S</button>
-                            <span>rR</span>
-                        </div>
+                        <div><button>A</button></div>
+                        <div><button>S</button></div>
                     </div>
 
                 </section>
