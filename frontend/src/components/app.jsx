e0740f67521ef22a05a35c04186749e9b317891b
chaitanyavd <chaitanya.v.de@gmail.com>
2019-07-14 18:29:39 -0700

    F this

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-
 import GameFormContainer from './game/game_form_container';
 import GameIndexContainer from './game/game_index_container';
+import Game from './game/game';
+
 
 const App = () => (
     <Switch>
         <Route exact path="/games/form" component={GameFormContainer} />
         <Route exact path="/games/" component={GameIndexContainer} />
+        <Route exact path="/games/test" component={Game} />
     </Switch>
 );
 
